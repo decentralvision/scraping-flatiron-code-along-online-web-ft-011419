93dddbeba02828475e04762c6be2cb2495a15b48
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-02 04:50:04 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -33,8 +33,7 @@ class Scraper
     end
   end
 
-  def print_courses
-    def print_courses
+def print_courses
   self.make_courses
   Course.all.each do |course|
     if course.title
@@ -42,7 +41,6 @@ class Scraper
       puts "  Schedule: #{course.schedule}"
       puts "  Description: #{course.description}"
     end
-  end
 end
 
   end
